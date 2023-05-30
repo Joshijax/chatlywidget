3f455321bb2c78997b6785356d8434cf50064e41
joshijax <joshijax08@gmail.com>
2023-05-30 15:43:13 +0100

    alpha test

--- chatly.js
+++ chatly.js
@@ -1,155 +1,142 @@
-function _0xf2a2(_0x1e9ab0, _0x3d47d6) {
-  var _0x48652b = _0x4865();
-  return (
-    (_0xf2a2 = function (_0xf2a291, _0x331b19) {
-      _0xf2a291 = _0xf2a291 - 0x8f;
-      var _0x3f6eff = _0x48652b[_0xf2a291];
-      return _0x3f6eff;
-    }),
-    _0xf2a2(_0x1e9ab0, _0x3d47d6)
-  );
-}
-(function (_0x4ae31a, _0x3524b0) {
-  var _0x114ea5 = _0xf2a2,
-    _0x480689 = _0x4ae31a();
-  while (!![]) {
-    try {
-      var _0x4c9d5e =
-        (parseInt(_0x114ea5(0xaa)) / 0x1) * (-parseInt(_0x114ea5(0xa0)) / 0x2) +
-        -parseInt(_0x114ea5(0x95)) / 0x3 +
-        (-parseInt(_0x114ea5(0x96)) / 0x4) * (parseInt(_0x114ea5(0x90)) / 0x5) +
-        (-parseInt(_0x114ea5(0x9d)) / 0x6) *
-          (-parseInt(_0x114ea5(0xad)) / 0x7) +
-        -parseInt(_0x114ea5(0x98)) / 0x8 +
-        (parseInt(_0x114ea5(0x93)) / 0x9) * (parseInt(_0x114ea5(0xb1)) / 0xa) +
-        (parseInt(_0x114ea5(0x9b)) / 0xb) * (parseInt(_0x114ea5(0xa9)) / 0xc);
-      if (_0x4c9d5e === _0x3524b0) break;
-      else _0x480689["push"](_0x480689["shift"]());
-    } catch (_0x4b293e) {
-      _0x480689["push"](_0x480689["shift"]());
-    }
-  }
-})(_0x4865, 0x3cd8a),
-  (function () {
-    var _0x127f16 = _0xf2a2,
-      _0x16af20 = _0x127f16(0xae),
-      _0x274571 = _0x127f16(0xab),
-      _0x28ce6a = _0x127f16(0x9c);
-    document[_0x127f16(0xa5)](_0x127f16(0x9e), function () {
-      var _0x90bf7d = _0x127f16,
-        _0x57226f = document[_0x90bf7d(0xb6)](_0x90bf7d(0x94));
-      (_0x57226f["id"] = "chatly-app"),
-        document[_0x90bf7d(0xb0)]["appendChild"](_0x57226f),
-        fetch(
-          "https://api.github.com/repos/" +
-            _0x16af20 +
-            "/" +
-            _0x274571 +
-            "/contents/js?ref=" +
-            _0x28ce6a
-        )
-          [_0x90bf7d(0x8f)](function (_0x21bbb4) {
-            return _0x21bbb4["json"]();
-          })
-          [_0x90bf7d(0x8f)](function (_0x42e682) {
-            _0x42e682["forEach"](function (_0x58a140) {
-              var _0x13298a = _0xf2a2;
-              if (
-                _0x58a140["type"] === _0x13298a(0x92) &&
-                _0x58a140[_0x13298a(0xa4)][_0x13298a(0xa2)](".js")
-              ) {
-                var _0x5ade90 = document[_0x13298a(0xb6)](_0x13298a(0xaf));
-                (_0x5ade90["src"] =
-                  _0x13298a(0xb5) +
-                  _0x16af20 +
-                  _0x13298a(0x97) +
-                  _0x274571 +
-                  _0x13298a(0xa6) +
-                  _0x58a140["name"]),
-                  document[_0x13298a(0xa7)][_0x13298a(0x9f)](_0x5ade90);
-              }
-            });
-          }),
-        fetch(
-          _0x90bf7d(0xa1) +
-            _0x16af20 +
-            "/" +
-            _0x274571 +
-            _0x90bf7d(0x99) +
-            _0x28ce6a
-        )
-          ["then"](function (_0x286867) {
-            var _0x5d4435 = _0x90bf7d;
-            return _0x286867[_0x5d4435(0xb3)]();
-          })
-          [_0x90bf7d(0x8f)](function (_0x58c68d) {
-            _0x58c68d["forEach"](function (_0xf90634) {
-              var _0x2a6556 = _0xf2a2;
-              if (
-                _0xf90634[_0x2a6556(0xa3)] === _0x2a6556(0x92) &&
-                _0xf90634[_0x2a6556(0xa4)]["endsWith"](_0x2a6556(0xa8))
-              ) {
-                var _0x238f98 = document[_0x2a6556(0xb6)](_0x2a6556(0xb4));
-                (_0x238f98[_0x2a6556(0x9a)] = _0x2a6556(0xb2)),
-                  (_0x238f98[_0x2a6556(0xa3)] = "text/css"),
-                  (_0x238f98[_0x2a6556(0xac)] =
-                    "https://" +
-                    _0x16af20 +
-                    ".github.io/" +
-                    _0x274571 +
-                    _0x2a6556(0x91) +
-                    _0xf90634[_0x2a6556(0xa4)]),
-                  document[_0x2a6556(0xa7)][_0x2a6556(0x9f)](_0x238f98);
-              }
-            });
-          });
-    });
-  })();
-function _0x4865() {
-  var _0x2bc061 = [
-    "file",
-    "171fbafMR",
-    "div",
-    "1090131PjEULb",
-    "15412zOllvo",
-    ".github.io/",
-    "2364488ZOiPvL",
-    "/contents/css?ref=",
-    "rel",
-    "11vUfbTv",
-    "master",
-    "152448KDmAqi",
-    "DOMContentLoaded",
-    "appendChild",
-    "925282sqAWZx",
-    "https://api.github.com/repos/",
-    "endsWith",
-    "type",
-    "name",
-    "addEventListener",
-    "/js/",
-    "head",
-    ".css",
-    "10722444UXyJda",
-    "1lSlNJv",
-    "chatlywidget",
-    "href",
-    "49Oteaus",
-    "joshijax",
-    "script",
-    "body",
-    "220450NjPaQR",
-    "stylesheet",
-    "json",
-    "link",
-    "https://",
-    "createElement",
-    "then",
-    "155LLHQnb",
-    "/css/",
-  ];
-  _0x4865 = function () {
-    return _0x2bc061;
-  };
-  return _0x4865();
-}
+(function () {
+  // Replace USERNAME with your GitHub username
+  var USERNAME = "joshijax";
+
+  // Replace REPOSITORY with the name of your repository
+  var REPOSITORY = "chatlywidget";
+
+  // Replace BRANCH with the name of the branch that contains your files (usually 'main' or 'master')
+  var BRANCH = "master";
+
+  // Wait for the document to finish loading
+  document.addEventListener("DOMContentLoaded", function () {
+    // Create a new div element with the "chatly-app" id and append it to the body
+    var chatlycon = document.createElement("div");
+    chatlycon.id = "chatly-container";
+    chatlycon.attachShadow({ mode: "open" });
+    document.body.appendChild(chatlycon);
+    // Create a Shadow Root for the app
+    var shadowRoot = chatlycon;
+
+    // Load all the JavaScript files from the scripts directory
+    fetch(
+      "https://api.github.com/repos/" +
+        USERNAME +
+        "/" +
+        REPOSITORY +
+        "/contents/js?ref=" +
+        BRANCH
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (files) {
+        files.forEach(function (file) {
+          if (file.type === "file" && file.name.endsWith(".js")) {
+            var scriptElement = document.createElement("script");
+            scriptElement.src =
+              "https://" +
+              USERNAME +
+              ".github.io/" +
+              REPOSITORY +
+              "/js/" +
+              file.name;
+            shadowRoot.appendChild(scriptElement);
+          }
+        });
+      });
+
+    // Load all the CSS files from the stylesheets directory
+    fetch(
+      "https://api.github.com/repos/" +
+        USERNAME +
+        "/" +
+        REPOSITORY +
+        "/contents/css?ref=" +
+        BRANCH
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (files) {
+        files.forEach(function (file) {
+          if (file.type === "file" && file.name.endsWith(".css")) {
+            var linkElement = document.createElement("link");
+            linkElement.rel = "stylesheet";
+            linkElement.type = "text/css";
+            linkElement.href =
+              "https://" +
+              USERNAME +
+              ".github.io/" +
+              REPOSITORY +
+              "/css/" +
+              file.name;
+            shadowRoot.appendChild(linkElement);
+          }
+        });
+      });
+
+    // Load all the MP3 files from the media directory
+    fetch(
+      "https://api.github.com/repos/" +
+        USERNAME +
+        "/" +
+        REPOSITORY +
+        "/contents/media?ref=" +
+        BRANCH
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (files) {
+        files.forEach(function (file) {
+          if (file.type === "file" && file.name.endsWith(".mp3")) {
+            var audioElement = document.createElement("audio");
+            audioElement.src =
+              "https://" +
+              USERNAME +
+              ".github.io/" +
+              REPOSITORY +
+              "/media/" +
+              file.name;
+            audioElement.preload = "auto";
+            shadowRoot.appendChild(audioElement);
+          }
+        });
+      });
+
+          // Load all the MP3 files from the media directory
+    fetch(
+      "https://api.github.com/repos/" +
+        USERNAME +
+        "/" +
+        REPOSITORY +
+        "/contents/media?ref=" +
+        BRANCH
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (files) {
+        files.forEach(function (file) {
+          if (file.type === "file" && file.name.endsWith(".mp3")) {
+            var audioElement = document.createElement("audio");
+            audioElement.src =
+              "https://" +
+              USERNAME +
+              ".github.io/" +
+              REPOSITORY +
+              "/media/" +
+              file.name;
+            audioElement.preload = "auto";
+            shadowRoot.appendChild(audioElement);
+          }
+        });
+      });
+
+    // Create a new div element inside the Shadow Root with the "chatly-app" id
+    var chatlyApp = document.createElement("div");
+    chatlyApp.id = "chatly-app";
+    document.body.appendChild(chatlyApp);
+  });
+})();
